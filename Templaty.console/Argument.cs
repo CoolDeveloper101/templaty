f973f4612c8f6b755a7f5e5e6186697805aaed7a
Ashwin Gupta <cooldeveloper101@gmail.com>
2020-07-28 11:38:01 +0530

    Rename Templaty.Console to Templaty.console

---
+++ Templaty.console/Argument.cs
@@ -0,0 +1,60 @@
+﻿namespace Templaty.console
+{
+    class Argument
+    {
+        public readonly string Command;
+        public readonly ArgumentType Type;
+        public readonly string Value;
+
+        public Argument(string value)
+        {
+            if (value.StartsWith("--"))
+            {
+                if (value.Length <= 2)
+                {
+                    Value = value;
+                    Type = ArgumentType.Any;
+                }
+                else if (value.Contains('='))
+                {
+                    value = value.Substring(2);
+                    Command = value[..value.IndexOf('=')];
+                    Value = value[(value.IndexOf('=') + 1)..];
+                    Type = ArgumentType.Variable;
+                }
+                else
+                {
+                    Command = value.Substring(2);
+                    Type = ArgumentType.Option;
+                }
+            }
+            else if (value.StartsWith('-'))
+            {
+                if (value.Length <= 1)
+                {
+                    Value = value;
+                    Type = ArgumentType.Any;
+                }
+                else
+                {
+                    Command = value.Substring(1);
+                    Type = ArgumentType.Option;
+                }
+            }
+            else
+            {
+                Value = value;
+                Type = ArgumentType.Any;
+            }
+        }
+
+        public override string ToString()
+        {
+            if (Type == ArgumentType.Variable)
+                return $"{Command}: {Value}";
+            else if (Type == ArgumentType.Option)
+                return $"{Type}: {Command}";
+            return $"{Type}: {Value}";
+        }
+    }
+}
---
+++ Templaty.console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Templaty.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Argument> parsedArgs = new List<Argument>();
+            foreach (var arg in args)
+            {
+                parsedArgs.Add(new Argument(arg));
+            }
+
+            var command = parsedArgs[0];
+            if (parsedArgs.Count == 1 && command.Type == ArgumentType.Option)
+            {
+                if (command.Command == "help" || command.Command == "h")
+                    Console.WriteLine(File.ReadAllText("./HelpFiles/help.txt"));
+            }
+            // Console.WriteLine($"[{string.Join(", ", parsedArgs)}]");
+        }
+    }
+}
